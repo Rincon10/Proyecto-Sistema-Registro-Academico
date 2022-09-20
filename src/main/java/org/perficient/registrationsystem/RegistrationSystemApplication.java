e19825ae3ea58b148ebd8664611e2e9b95604848
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-09-19 23:08:47 -0500

    Base code project

---
+++ src/main/java/org/perficient/registrationsystem/RegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.perficient.registrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perficient/registrationsystem/RegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perficient.registrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

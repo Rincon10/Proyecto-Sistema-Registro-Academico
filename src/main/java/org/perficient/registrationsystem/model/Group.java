c255288f786043eada357acdfec3d3df030d9854
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-10-03 17:59:17 -0500

    Updating Group with new attributes and constraints

--- src/main/java/org/perficient/registrationsystem/model/Group.java
+++ src/main/java/org/perficient/registrationsystem/model/Group.java
@@ -1,11 +1,13 @@
 package org.perficient.registrationsystem.model;
 
 import lombok.Data;
+import org.perficient.registrationsystem.dto.GroupDto;
+import org.perficient.registrationsystem.mappers.ProfessorMapper;
+import org.perficient.registrationsystem.mappers.SubjectMapper;
 
 import javax.persistence.*;
 import javax.validation.constraints.Positive;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.Time;
 
 /**
  * Class Group created on 9/19/2022
@@ -15,7 +17,8 @@ import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "groups")
+@Table(name = "groups",
+        uniqueConstraints = {@UniqueConstraint(name = "UniqueAcronymAndNumber", columnNames = {"subject_acronym", "number"})})
 public class Group extends BaseEntity {
 
     @Id
@@ -30,12 +33,24 @@ public class Group extends BaseEntity {
     private Professor professor;
 
     @ManyToOne
-    @JoinColumn(name = "subject_id")
+    @JoinColumn(name = "subject_acronym")
     private Subject subject;
 
-    @ManyToMany
-    @JoinTable(name = "student_group", joinColumns = @JoinColumn(name = "group_id"),
-            inverseJoinColumns = @JoinColumn(name = "student_id"))
-    private Set<Student> students = new HashSet<>();
+    private Time startTime;
+    private Time endTime;
+
+    public void update(GroupDto groupDto) {
+        this.setId(groupDto.getId());
+        this.setNumber(groupDto.getNumber());
+        this.setProfessor(ProfessorMapper.INSTANCE.professorDtoToProfessor(groupDto.getProfessor()));
+        this.setSubject(SubjectMapper.INSTANCE.subjectDtoToSubject(groupDto.getSubject()));
+        this.setStartTime(groupDto.getStartTime());
+        this.setEndTime(groupDto.getEndTime());
+
+    }
+//    @ManyToMany
+//    @JoinTable(name = "student_group", joinColumns = @JoinColumn(name = "group_id"),
+//            inverseJoinColumns = @JoinColumn(name = "student_id"))
+//    private Set<Student> students = new HashSet<>();
 
 }

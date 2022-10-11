73c63eac5081162b52c0e64b70e2577e5795d0be
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-10-11 12:56:32 -0500

    Adding development and production configuration files

--- src/main/java/org/perficient/registrationsystem/controllers/GroupController.java
+++ src/main/java/org/perficient/registrationsystem/controllers/GroupController.java
@@ -1,20 +1,12 @@
 package org.perficient.registrationsystem.controllers;
 
 import org.perficient.registrationsystem.dto.GroupDto;
-import org.perficient.registrationsystem.dto.server.ServerResponseDto;
 import org.perficient.registrationsystem.services.GroupService;
-import org.perficient.registrationsystem.services.exceptions.ServerErrorException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Class GroupController Created on 29/09/2022
@@ -28,18 +20,6 @@ public class GroupController {
 
     public GroupController(GroupService groupService) {
         this.groupService = groupService;
-    }
-
-    private void throwExceptionForInvalidData(BindingResult result) throws ServerErrorException {
-        List<String> errors = new ArrayList<>();
-
-        result.getFieldErrors()
-                .stream()
-                .map(f -> "The field " + f.getField() + " " + f.getDefaultMessage())
-                .forEach(errors::add);
-
-        throw new ServerErrorException(errors.toString(), HttpStatus.BAD_REQUEST.value());
-
     }
 
     //GET
@@ -63,9 +43,7 @@ public class GroupController {
     @PostMapping
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
-    public Boolean addGroup(@Valid @RequestBody GroupDto groupDto, BindingResult result) throws Exception {
-        if (result.hasErrors()) throwExceptionForInvalidData(result);
-
+    public Boolean addGroup(@Valid @RequestBody GroupDto groupDto) throws Exception {
         return groupService.addGroup(groupDto);
     }
 
@@ -74,12 +52,8 @@ public class GroupController {
     @PutMapping(path = "/{id}")
     @ResponseBody
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public GroupDto updateGroupById(@PathVariable Integer id,
-                                    @Valid @RequestBody GroupDto groupDto, BindingResult result) throws Exception {
-        if (result.hasErrors()) throwExceptionForInvalidData(result);
-
+    public GroupDto updateGroupById(@PathVariable Integer id, @Valid @RequestBody GroupDto groupDto) throws Exception {
         return groupService.updateGroupById(id, groupDto);
-
     }
 
     //DELETE
@@ -88,17 +62,5 @@ public class GroupController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Boolean deleteById(@PathVariable Integer id) throws Exception {
         return groupService.deleteGroupById(id);
-    }
-
-    //HANDLER EXCEPTION
-
-    @ExceptionHandler(ServerErrorException.class)
-    public ResponseEntity<?> serverErrorResponseHandlerException(ServerErrorException e) {
-        String message = e.getMessage();
-        int httpStatus = e.getHttpStatus();
-
-        Logger.getLogger(SubjectController.class.getName()).log(Level.SEVERE, null, message);
-
-        return new ResponseEntity<>(new ServerResponseDto(message, httpStatus), HttpStatus.valueOf(httpStatus));
     }
 }

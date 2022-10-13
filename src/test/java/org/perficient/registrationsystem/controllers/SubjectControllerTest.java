b1212483da319450114a494dc312c9e07b3a9752
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-10-13 10:14:10 -0500

    Adding SubjectControllerTest base code

---
+++ src/test/java/org/perficient/registrationsystem/controllers/SubjectControllerTest.java
@@ -0,0 +1,37 @@
+package org.perficient.registrationsystem.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.Assert.*;
+
+@AutoConfigureJsonTesters
+@WebMvcTest(controllers = GroupController.class)
+public class SubjectControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAllSubjects() {
+    }
+
+    @Test
+    public void getSubjectByAcronym() {
+    }
+
+    @Test
+    public void addSubject() {
+    }
+
+    @Test
+    public void updateSubjectByAcronym() {
+    }
+
+    @Test
+    public void deleteByAcronym() {
+    }
+}

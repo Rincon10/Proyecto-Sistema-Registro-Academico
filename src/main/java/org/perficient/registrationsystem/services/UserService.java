daf743569994a865e11801162fe85fb2496b1afd
Rincon10 <ivan.rincon-s@mail.escuelaing.edu.co>
2022-10-18 16:15:25 -0500

    Defining the services to the users CRUD

--- src/main/java/org/perficient/registrationsystem/services/UserService.java
+++ src/main/java/org/perficient/registrationsystem/services/UserService.java
@@ -2,16 +2,24 @@ package org.perficient.registrationsystem.services;
 
 import org.perficient.registrationsystem.dto.UserDto;
 
-import java.util.Set;
-
 /**
  * Interface UserService Created on 22/09/2022
  *
  * @Author Iván Camilo Rincon Saavedra
  */
 public interface UserService {
-    Set<UserDto> getUsers();
 
-    UserDto addUser(UserDto userDto);
+    //GET
+    UserDto getUserByEmail() throws Exception;
 
+    //POST
+    UserDto addUser(UserDto userDto) throws Exception;
+
+    //UPDATE
+    UserDto updateUserById(int id, UserDto userDto) throws Exception;
+
+    UserDto updateUserByEmail(String email, UserDto userDto) throws Exception;
+
+    //DELETE
+    Boolean deleteByEmail(String email) throws Exception;
 }
